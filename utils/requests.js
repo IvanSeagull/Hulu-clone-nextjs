0e9b53fecbe3be71e17a650f0ad354a506ab4c3b
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-02-17 18:30:03 +0000

    Finished Nav component

--- utils/requests.js
+++ utils/requests.js
@@ -1,6 +1,6 @@
 const API_KEY = process.env.API_KEY;
 
-export default {
+const requests = {
   fetchTrending: {
     title: 'Trending',
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -47,3 +47,5 @@ export default {
     url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
   },
 };
+
+export default requests;
